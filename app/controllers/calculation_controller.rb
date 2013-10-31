33e822ac2a1c99446229ebfdf47c1ba703851bd9
Richard J Dube <rdube2014@kellogg.northwestern.edu>
2013-10-31 13:14:48 -0500

    Midterm Practice

--- app/controllers/calculation_controller.rb
+++ app/controllers/calculation_controller.rb
@@ -1,12 +1,27 @@
 class CalculationController < ApplicationController
   def home
-  end
+  	end
 
-  def square_root
+def square
+  	@the_number = params[:number].to_i
+
+  	@answer = @the_number * @the_number
+  	  end
+
+  def sqrt
     @the_number = params[:number].to_f
+
     @answer = Math.sqrt(@the_number)
   end
 
   def pmt
+  	@interest_rate = params[:interest_rate].to_f
+  	@number_of_payments = params[:number_of_payments].to_f
+  	@present_value = params[:present_value].to_f
+
+  	numerator = (@present_value * @interest_rate/100/12) * ((1 + @interest_rate/100/12)**@number_of_payments)
+  	denominator = ((1 + @interest_rate/100/12)**@number_of_payments) - 1
+
+  	@payment = numerator / denominator
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Practice::Application.routes.draw do
-  get("/", { :controller => "calculations", :action => "home" })
-  get("/square_root/:number", { :controller => "calculations", :action => "sqrt" })
-  get("/payment/:interest_rate/:number_of_payments/:present_value", { :controller => "calculations", :action => "pmt" })
+  get("/", { :controller => "calculation", :action => "home" })
+  get("/square/:number", { :controller => "calculation", :action => "square" })
+  get("/square_root/:number", { :controller => "calculation", :action => "sqrt" })
+  get("/payment/:interest_rate/:number_of_payments/:present_value", { :controller => "calculation", :action => "pmt" })
 end
